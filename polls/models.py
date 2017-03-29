5c0dbd621aeffdd4f52c3541a12d6b26387cb011
Adriana Matsufuji <adrianabm@gmail.com>
2017-03-29 16:51:10 +0200

    Create Model tests

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,17 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.utils import timezone
+
+import datetime
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
